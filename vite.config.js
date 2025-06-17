c060407af041712eee85ecab4586013293df0b8b
Danila <danil.simonov228@mail.ru>
2025-06-17 22:08:33 +0300

    hi

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Определяем базу в зависимости от среды
+const isProduction = process.env.NODE_ENV === 'production'
+
 export default defineConfig({
   plugins: [react()],
+  base: isProduction ? '/MasterInk/' : '/', // <-- магия тут
 })
